54d0bf1af10959294ef631723b2beef87f8cc85e
AbhayShaw0301 <abhayshaw0301@gmail.com>
2024-04-04 04:01:21 -0700

    added open close feature on mobile

--- src/components/BrandSection.tsx
+++ src/components/BrandSection.tsx
@@ -7,11 +7,11 @@ export default function BrandSection() {
 
   return (
     <div className="flex flex-col bg-backdrop items-center mt-0 pt-6 sm:pt-16 px-4 sm:px-0">
-      <p className="text-2xl text-center text-secondary font-bold sm:pb-8">
+      <p className="text-2xl text-center text-secondary font-bold pb-8">
         Subscribers scheduling from more than <br />{" "}
         <strong className="text-primary">1,000,000 </strong>users worldwide
       </p>
-      <div className="sm:flex hidden justify-center mx-auto mt-4 sm:max-w-[1200px] overflow-hidden">
+      <div className="sm:flex hidden  justify-center mx-auto mt-4 sm:max-w-[1200px] overflow-hidden">
         {imageUrls.map((imageUrl, index) => (
           <img
             key={index}
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,8 @@
+
 import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdClose } from "react-icons/md";
 import mainLogo from "../assets/logo.svg";
-
 
 export default function NavBar() {
   const [openMenu, setOpenMenu] = useState<boolean>(false);
@@ -29,7 +29,7 @@ export default function NavBar() {
             ))}
           </ul>
         </div>
-        <div className="login-section sm:flex py-2 items-center sm:px-4">
+        <div className="login-section sm:flex py-2 items-center sm:px-4 relative">
           {openMenu ? (
             <MdClose
               className="sm:hidden hover:cursor-pointer"
@@ -41,19 +41,23 @@ export default function NavBar() {
               onClick={() => setOpenMenu(!openMenu)}
             />
           )}
-          {openMenu && (
-            <div className="sm:hidden w-full h-[100%] flex flex-col bg-white absolute top-8 right-0">
-              {menuItems.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </div>
-          )}
+        
           <p className="rounded-lg sm:px-4 py-2 hidden text-secondary sm:block font-bold">Log In</p>
           <button className="bg-primary text-white font-bold text-sm rounded-3xl px-4 py-3 hidden sm:block">
             Get Started
           </button>
         </div>
       </div>
+      
+      {openMenu && (
+        <div className="absolute w-full left-1/2  -translate-x-1/2    bg-gray-100 pt-2 pb-4 -translate-y-6 border shadow-xl">
+          <ul className="flex flex-col justify-between text-secondary font-bold px-4">
+            {menuItems.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
